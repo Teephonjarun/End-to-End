746ff1cb4cd342695730afded0d910fc6f6cb696
Teephonjarun <156630016+Teephonjarun@users.noreply.github.com>
2024-07-02 15:08:22 +0700

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "jel-bg"
     region         = "ap-northeast-2"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "jel-tabe"
+    dynamodb_table = "gel-table"
     encrypt        = true
   }
   required_version = ">=0.13.0"
